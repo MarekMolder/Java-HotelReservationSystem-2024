4fe2ec68bb15b2c6089b0583b7a0e55a0c3e758c
marek <marekmolder@gmail.com>
2024-02-08 22:15:21 +0200

    Test

--- EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowserTest.java
+++ EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowserTest.java
@@ -23,8 +23,8 @@ public class WebBrowserTest {
         webBrowser.goTo("facebook.com");
         webBrowser.goTo("amazon.com");
         webBrowser.back();
-        List<String> expected = new ArrayList<>
-                (Arrays.asList("google.com", "facebook.com", "amazon.com", "facebook.com"));
+        List<String> expected = new ArrayList<>(
+                Arrays.asList("google.com", "facebook.com", "amazon.com", "facebook.com"));
         List<String> actual = webBrowser.getHistory();
         assertEquals(expected, actual, () -> String.format("Expected: '%s' , but got '%s'", expected, actual));
     }
