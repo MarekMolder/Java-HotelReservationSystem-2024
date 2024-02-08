57c5f9c01208e5e509697ed43d156c0bd868ded8
marek <marekmolder@gmail.com>
2024-02-08 22:13:13 +0200

    EX2 v3

---
+++ EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowserTest.java
@@ -0,0 +1,30 @@
+package ee.taltech.iti0202.webbrowser;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WebBrowserTest {
+    @Test
+    public void testSetHomePage() {
+        WebBrowser webBrowser = new WebBrowser();
+        webBrowser.setHomePage("neti.ee");
+        String expected = "neti.ee";
+        String actual = webBrowser.getHomePage();
+        assertEquals(expected, actual, () -> String.format("Expected: '%s' , but got '%s'", expected, actual));
+    }
+    @Test
+    public void testBackSavesHistory() {
+        WebBrowser webBrowser = new WebBrowser();
+        webBrowser.goTo("facebook.com");
+        webBrowser.goTo("amazon.com");
+        webBrowser.back();
+        List<String> expected = new ArrayList<>(Arrays.asList("google.com", "facebook.com", "amazon.com", "facebook.com"));
+        List<String> actual = webBrowser.getHistory();
+        assertEquals(expected, actual, () -> String.format("Expected: '%s' , but got '%s'", expected, actual));
+    }
+}
