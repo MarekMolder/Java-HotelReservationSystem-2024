9e674f40a535e434f35ea986a0979c2ba9fecd1c
marek <marekmolder@gmail.com>
2024-02-17 00:50:09 +0200

    EX3 v7

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Person.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Person.java
@@ -8,20 +8,38 @@ public class Person {
     private Integer money;
     private List<Book> books = new ArrayList<>();
 
+    /**
+     * Constructs a new person with the specified name, money and list of books he own.
+     * @param name
+     * @param money
+     */
     public Person(String name, int money) {
         this.name = name;
         this.money = money;
         this.books = books;
     }
 
+    /**
+     * Method to get an overview of persons funds.
+     * @return money.
+     */
     public int getMoney() {
         return money;
     }
 
+    /**
+     * Method to get persons name.
+     * @return name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Method for buying a book.
+     * @param book
+     * @return true || false.
+     */
     public boolean buyBook(Book book) {
         if (book != null && this.getMoney() >= book.getPrice() && book.getOwner() == null) {
             this.money -= book.getPrice();
@@ -37,6 +55,11 @@ public class Person {
         return false;
     }
 
+    /**
+     * Method for selling a book.
+     * @param book
+     * @return true || false.
+     */
     public boolean sellBook(Book book) {
         if (book != null && book.getOwner() == this) {
             this.money += book.getPrice();
@@ -47,6 +70,10 @@ public class Person {
         return false;
     }
 
+    /**
+     * Method to get persons books.
+     * @return person books.
+     */
     public List<Book> getBooks() {
         return this.books;
     }
