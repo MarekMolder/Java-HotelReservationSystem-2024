05d2a7a7e03c571c07d6d02abdeaeb8531dbd032
marek <marekmolder@gmail.com>
2024-03-03 15:21:15 +0200

    ex5 try 1

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/SpaceOven.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/SpaceOven.java
@@ -56,7 +56,6 @@ public class SpaceOven extends Oven implements Fixable{
                     } else {
                         getResourceStorage().takeResource("star essence", 10 * Multiplier);
                     }
-                    this.orbs = 0;
                     this.fixed++;
                 }
             }
