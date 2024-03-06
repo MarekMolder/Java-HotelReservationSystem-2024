ba24255b702ba2900abc0b3b65e4481d27e89f70
marek <marekmolder@gmail.com>
2024-03-06 19:44:30 +0200

    ex5 try 1

--- PR/PR06Shelter/src/ee/taltech/iti0202/shelter/animalprovider/TestAnimalProvider.java
+++ PR/PR06Shelter/src/ee/taltech/iti0202/shelter/animalprovider/TestAnimalProvider.java
@@ -1,13 +1,16 @@
 package ee.taltech.iti0202.shelter.animalprovider;
 
 import ee.taltech.iti0202.shelter.animal.Animal;
+import ee.taltech.iti0202.shelter.animal.Hirola;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestAnimalProvider implements AnimalProvider {
     @Override
     public List<Animal> provide(Animal.Type type) {
-        // here some code
-        return null;
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Hirola("red"));
+        return animals;
     }
 }
