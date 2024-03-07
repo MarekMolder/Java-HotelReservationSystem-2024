93da516cce8c9d5f74e906f850bba4f2a17a901e
marek <marekmolder@gmail.com>
2024-03-07 14:33:56 +0200

    ex6 try 1

---
+++ EX/EX06Files/src/ee/taltech/iti0202/files/output/OutputFilesWriter.java
@@ -0,0 +1,24 @@
+package ee.taltech.iti0202.files.output;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class OutputFilesWriter {
+
+    public boolean writeLinesToFile(List<String> lines, String filename) {
+        if (filename.isEmpty()) {
+            return false;
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (String text : lines) {
+                writer.write(text);
+                writer.newLine();
+            }
+            return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
