459c2c8774963287e515a7dfeac710c56093b4c8
maremo <maremo@taltech.ee>
2024-03-20 15:54:41 +0200

    ex7 builder

--- EX/EX07Zoo/src/ee/taltech/iti0202/zoo/animal/LambBuilder.java
+++ EX/EX07Zoo/src/ee/taltech/iti0202/zoo/animal/LambBuilder.java
@@ -4,16 +4,30 @@ public class LambBuilder {
     private String name;
     private Integer hungry;
 
+    /**
+     * Method to set name.
+     * @param name
+     * @return
+     */
     public LambBuilder setName(String name) {
         this.name = name;
         return this;
     }
 
+    /**
+     * Method to set hungry.
+     * @param hungry
+     * @return
+     */
     public LambBuilder setHungry(Integer hungry) {
         this.hungry = hungry;
         return this;
     }
 
+    /**
+     * Method to create lamb.
+     * @return
+     */
     public Lamb createLamb() {
         return new Lamb(name, hungry);
     }
