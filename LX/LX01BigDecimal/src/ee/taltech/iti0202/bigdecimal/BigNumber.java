56bbe611ece2a7049d1c28548f905e73a23af7d3
marek <marekmolder@gmail.com>
2024-03-24 20:35:36 +0200

    LX01 TRY

---
+++ LX/LX01BigDecimal/src/ee/taltech/iti0202/bigdecimal/BigNumber.java
@@ -0,0 +1,161 @@
+package ee.taltech.iti0202.bigdecimal;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class BigNumber {
+    /**
+     * Multiply 2 int values and return product in BigInteger
+     * @param factor1 first factor
+     * @param factor2 second factor
+     * @return factor1 x factor2
+     */
+    public BigInteger multiplyBigInteger(int factor1, int factor2) {
+        return BigInteger.valueOf((long) factor1 * factor2);
+    }
+
+    /**
+     * Divide 2 values and return quotient in BigInteger
+     * If divisor is 0, return 0
+     * @param dividend dividend
+     * @param divisor divisor
+     * @return dividend / divisor
+     */
+    public BigInteger divideBigInteger(BigInteger dividend, int divisor) {
+        if (divisor == 0) {
+            return BigInteger.ZERO; // return 0 if divisor is 0
+        } else {
+            return dividend.divide(BigInteger.valueOf(divisor));
+        }
+    }
+
+    /**
+     * Add 2 values and return sum in BigInteger
+     * @param add1 first value
+     * @param add2 second value
+     * @return add1 + add2
+     */
+    public BigInteger addBigInteger(int add1, int add2) {
+        return BigInteger.valueOf(add1 + add2);
+    }
+
+    /**
+     * Subtract 2 values and return difference in BigInteger
+     * @param minuend first value
+     * @param subtrahend second value
+     * @return value1 - value2
+     */
+    public BigInteger subtractBigInteger(BigInteger minuend, int subtrahend) {
+        return minuend.subtract(BigInteger.valueOf(subtrahend));
+    }
+
+    /**
+     * Multiply double value with int multiplier, round according to rounding
+     * and return in BigDecimal
+     * @param value value to calculate
+     * @param multiplier multiplier to use
+     * @param rounding rounding to use
+     * @return value multiplied by multiplier and rounded by rounding
+     */
+    public BigDecimal multiplyAndRoundBigDecimal(double value, int multiplier, int rounding) {
+        BigDecimal result = BigDecimal.valueOf(value).multiply(BigDecimal.valueOf(multiplier));
+        return result.setScale(0, rounding);
+    }
+
+    /**
+     * Calculate n factorial and return in BigInteger
+     * If n < 0, it should return 1
+     * @param n n-th factorial to calculate
+     * @return n-th factorial
+     */
+    public BigInteger factorial(int n) {
+        if (n < 0) {
+            return BigInteger.ONE;
+        } else if (n == 0) {
+            return BigInteger.ONE;
+        } else {
+            BigInteger result = BigInteger.ONE;
+            for (int i = 1; i <= n; i++) {
+                result = result.multiply(BigInteger.valueOf(i));
+            }
+            return result;
+        }
+    }
+
+    /**
+     * Calculate base to the power of exponent and return in BigInteger
+     * @param base base
+     * @param exponent exponent
+     * @return base to the power of exponent
+     */
+    public BigInteger power(int base, int exponent) {
+        if (exponent < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (exponent == 0) {
+            return BigInteger.ONE;
+        }
+
+        BigInteger result = BigInteger.ONE;
+
+        for (int i = 0; i < exponent; i++) {
+            result = result.multiply(BigInteger.valueOf(base));
+        }
+
+        return result;
+    }
+
+    /**
+     * Round val1 and val2 with the rounding given, and check if they are equal after that
+     * @param val1 first value to round
+     * @param val2 second value to round
+     * @param rounding rounding to use
+     * @return true or false if val1 and val2 are equal after rounded with rounding
+     */
+    public boolean isSame(BigDecimal val1, BigDecimal val2, int rounding) {
+        BigDecimal roundedVal1 = val1.setScale(0, rounding);
+        BigDecimal roundedVal2 = val2.setScale(0, rounding);
+        return roundedVal1.equals(roundedVal2);
+    }
+
+    /**
+     *
+     * @param n n-th fib number to calculate
+     * @return n-th fib number value
+     */
+    public BigInteger fibonacci(int n) {
+        if (n == 0) {
+            return BigInteger.ZERO;
+        } else if (n == 1) {
+            return BigInteger.ONE;
+        } else {
+            return fibonacci(n - 1).add(fibonacci(n - 2));
+        }
+    }
+
+    /**
+     *
+     * @param n n-th luc number to calculate
+     * @return n-th lucas number
+     */
+    public BigInteger lucas(int n) {
+        if (n == 0) {
+            return BigInteger.valueOf(2);
+        } else if (n == 1) {
+            return BigInteger.ONE;
+        } else {
+            BigInteger lucasPrev = BigInteger.valueOf(2);
+            BigInteger lucasCurrent = BigInteger.ONE;
+            BigInteger lucasNext = BigInteger.ZERO;
+
+            for (int i = 2; i <= n; i++) {
+                lucasNext = lucasPrev.add(lucasCurrent);
+                lucasPrev = lucasCurrent;
+                lucasCurrent = lucasNext;
+            }
+
+            return lucasNext;
+        }
+    }
+}
