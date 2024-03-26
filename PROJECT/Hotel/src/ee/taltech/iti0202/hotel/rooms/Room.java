3b7f220678efdac9af6e467633f0bb22a927ff29
marek <marekmolder@gmail.com>
2024-03-26 17:11:14 +0200

    PROJECT FINISHED

--- PROJECT/Hotel/src/ee/taltech/iti0202/hotel/rooms/Room.java
+++ PROJECT/Hotel/src/ee/taltech/iti0202/hotel/rooms/Room.java
@@ -6,6 +6,7 @@ import ee.taltech.iti0202.hotel.hotel.Hotel;
  * Represents a room which will be added in hotel.
  */
 public class Room {
+    public static final int PRICE = 40; // Room price
     private static int nextRoomNumber = 1; // A unique number for every room
     private final int number; // A unique number for room
     int price; // The room price
@@ -25,7 +26,7 @@ public class Room {
      */
     public Room() {
         this.number = getAndIncrementNextRoomNumber();
-        this.price = 40;
+        this.price = PRICE;
         this.hotel = null;
     }
 
