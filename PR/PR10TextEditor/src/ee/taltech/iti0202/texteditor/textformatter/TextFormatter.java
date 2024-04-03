8078431c888f197fd691a2a657ff38e3d66007d1
marek <marekmolder@gmail.com>
2024-04-03 20:38:34 +0300

    pr10

---
+++ PR/PR10TextEditor/src/ee/taltech/iti0202/texteditor/textformatter/TextFormatter.java
@@ -0,0 +1,5 @@
+package ee.taltech.iti0202.texteditor.textformatter;
+
+public interface TextFormatter {
+    String format(String text);
+}
---
+++ PR/PR10TextEditor/src/ee/taltech/iti0202/texteditor/textformatter/UppercaseFormatter.java
@@ -0,0 +1,9 @@
+package ee.taltech.iti0202.texteditor.textformatter;
+
+public class UppercaseFormatter implements TextFormatter {
+
+    @Override
+    public String format(String text) {
+        return text.toUpperCase();
+    }
+}
