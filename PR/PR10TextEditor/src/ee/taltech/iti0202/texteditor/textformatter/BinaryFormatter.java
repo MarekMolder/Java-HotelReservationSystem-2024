dc33c26faf9e8ef6dcc6db94783dadbfec4b518c
marek <marekmolder@gmail.com>
2024-04-03 23:12:05 +0300

    pr10

--- PR/PR10TextEditor/src/ee/taltech/iti0202/texteditor/textformatter/BinaryFormatter.java
+++ PR/PR10TextEditor/src/ee/taltech/iti0202/texteditor/textformatter/BinaryFormatter.java
@@ -12,6 +12,6 @@ public class BinaryFormatter implements TextFormatter {
             result.append(binary);
         }
 
-        return result.toString() + "\n";
+        return result.toString();
     }
 }
