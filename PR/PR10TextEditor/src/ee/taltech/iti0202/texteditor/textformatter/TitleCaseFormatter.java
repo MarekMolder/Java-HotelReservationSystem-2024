93177de94a1732b1668edf0d9fc628f2969c436a
marek <marekmolder@gmail.com>
2024-04-03 21:09:06 +0300

    pr10

--- PR/PR10TextEditor/src/ee/taltech/iti0202/texteditor/textformatter/TitleCaseFormatter.java
+++ PR/PR10TextEditor/src/ee/taltech/iti0202/texteditor/textformatter/TitleCaseFormatter.java
@@ -20,7 +20,7 @@ public class TitleCaseFormatter implements TextFormatter {
                 result.append(words[i].substring(0, 1).toUpperCase()).append(words[i].substring(1)).append(" ");
             }
         }
-        result.append(words[words.length -1].substring(0, 1).toUpperCase()).append(words[0].substring(1)).append(" ");
+        result.append(words[words.length -1].substring(0, 1).toUpperCase()).append(words[words.length - 1].substring(1)).append(" ");
         return result.toString().trim();
     }
 }
