cd27276b33c57b7719db49f1cb89c6db6839a6ff
marek <marekmolder@gmail.com>
2024-05-24 16:13:20 +0300

    LX04

--- LX/LX04ComputerBuilder/src/ee/taltech/iti0202/computerbuilder/computer/Pc.java
+++ LX/LX04ComputerBuilder/src/ee/taltech/iti0202/computerbuilder/computer/Pc.java
@@ -3,7 +3,18 @@ package ee.taltech.iti0202.computerbuilder.computer;
 import ee.taltech.iti0202.computerbuilder.components.Component;
 
 public class Pc extends Computer {
-    public Pc(Component cpu, Component gpu, Component ram, Component motherboard, Component storage, Component psu, Component pcCase) {
+    /**
+     * Constructs a new pc.
+     * @param cpu
+     * @param gpu
+     * @param ram
+     * @param motherboard
+     * @param storage
+     * @param psu
+     * @param pcCase
+     */
+    public Pc(Component cpu, Component gpu, Component ram, Component motherboard,
+              Component storage, Component psu, Component pcCase) {
         super(cpu, gpu, ram, motherboard, storage, psu, pcCase);
     }
 }
