854b2df484b2c6ec2023718c2b4d29bd3e1ce028
marek <marekmolder@gmail.com>
2024-05-25 13:42:43 +0300

    exam 0 50+50

--- EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Timetable.java
+++ EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Timetable.java
@@ -1,4 +1,22 @@
 package ee.taltech.iti0202.exam.timetable;
 
+import java.util.List;
+import java.util.Optional;
+
 public class Timetable {
+    public Timetable() {
+
+    }
+
+    public Optional<String> addTask(String name, int day, int duration, boolean priority) {
+        return Optional.empty();
+    }
+
+    public boolean markTaskDone(String taskNumber) {
+        return false;
+    }
+
+    public List<String> getTasksForDay(int day) {
+        return null;
+    }
 }
