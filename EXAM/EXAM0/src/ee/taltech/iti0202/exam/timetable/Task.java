503d28fa26a464af700a93d128daa6391ca56846
marek <marekmolder@gmail.com>
2024-05-25 14:51:55 +0300

    exam 0 50+50

---
+++ EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Task.java
@@ -0,0 +1,68 @@
+package ee.taltech.iti0202.exam.timetable;
+
+public class Task {
+    private String name;
+    private final String id;
+    private int duration;
+    private boolean priority;
+    private int day;
+    private boolean isItDone;
+    private static int nextId = 1;
+
+    public String getId() {
+        return id;
+    }
+
+    public Task(String name, int day, int duration, boolean priority) {
+        this.id = getAndIncrementNextId();
+        this.name = name;
+        this.day = day;
+        this.duration = duration;
+        this.priority = priority;
+        this.isItDone = false;
+    }
+
+    public String getAndIncrementNextId() {
+        return "T" + nextId++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Boolean getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Boolean priority) {
+        this.priority = priority;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public boolean isItDone() {
+        return isItDone;
+    }
+
+    public void setItDone(boolean isItDone) {
+        this.isItDone = isItDone;
+    }
+}
--- EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Timetable.java
+++ EXAM/EXAM0/src/ee/taltech/iti0202/exam/timetable/Timetable.java
@@ -1,22 +1,67 @@
 package ee.taltech.iti0202.exam.timetable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class Timetable {
-    public Timetable() {
+    private List<Task> tasks;
 
+    public Timetable() {
+        List<Task> tasks = new ArrayList<>();
     }
 
     public Optional<String> addTask(String name, int day, int duration, boolean priority) {
+        Task task = new Task(name, day, duration, priority);
+        if (checkCanAddTask(task)) {
+            tasks.add(task);
+            return Optional.of(task.getName());
+        }
         return Optional.empty();
     }
 
     public boolean markTaskDone(String taskNumber) {
+        for (Task task : tasks) {
+            if (task.getId().equals(taskNumber) && !task.isItDone()) {
+                task.setItDone(true);
+                return true;
+            }
+        }
         return false;
     }
 
     public List<String> getTasksForDay(int day) {
-        return null;
+        List<String> result = new ArrayList<>();
+
+        if (day >= 1) {
+            for (Task task : tasks) {
+                if (task.getDay() == day && !task.isItDone()) {
+                    if (task.getPriority()) {
+                        result.addFirst(task.getId() + task.getName());
+                    }
+                    result.addLast(task.getId() + task.getName());
+                }
+            }
+            return result;
+        }
+        return new ArrayList<>();
+    }
+
+    public boolean checkCanAddTask(Task task) {
+        int total = 0;
+        if (task.getDuration() >= 1 && task.getDay() >= 1) {
+           for (Task t : tasks) {
+               if (t.getDay() == task.getDay()) {
+                   if (t.getName().equals(task.getName())) {
+                       return false;
+                   }
+                   total += t.getDuration();
+               }
+           }
+           if (total + task.getDuration() <= 5) {
+               return true;
+           }
+        }
+        return false;
     }
 }
